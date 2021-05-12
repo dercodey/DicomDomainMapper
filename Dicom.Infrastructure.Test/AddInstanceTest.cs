d5ab2ed0cde8b08c05fca9ee5f57e506f5d9bb58
Lane, Derek G <Derek.Lane@elekta.com>
2021-05-12 15:42:13 -0500

    adding test logic and initialization to clear out tables

--- Dicom.Infrastructure.Test/AddInstanceTest.cs
+++ Dicom.Infrastructure.Test/AddInstanceTest.cs
@@ -1,5 +1,6 @@
 using Microsoft.Data.SqlClient;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
 using DomainModel = Dicom.Domain.Model;
 
 namespace Dicom.Infrastructure.Test
@@ -7,55 +8,69 @@ namespace Dicom.Infrastructure.Test
     [TestClass]
     public class AddInstanceTest
     {
+        string connectionString =
+            @"Data Source=(localdb)\ProjectsV13;Initial Catalog=MyStoreDB;";
+
         [TestInitialize]
         public void SetupTest()
         {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                SqlCommand deleteAttributes = new SqlCommand("delete from DicomAttributes", connection);
+                deleteAttributes.ExecuteNonQuery();
+
+                SqlCommand deleteInstances = new SqlCommand("delete from DicomInstances", connection);
+                deleteInstances.ExecuteNonQuery();
+
+                SqlCommand deleteSeries = new SqlCommand("delete from DicomSeries", connection);
+                deleteSeries.ExecuteNonQuery();
+            }
         }
 
         [TestMethod]
         public void TestCreatingSeries()
         {
+            // construct a new series object and store the UID
+            var newSeriesDomainModel = TestData.CreateSeries();
+            var newSeriesUid = newSeriesDomainModel.SeriesInstanceUid;
+
             // NOTE that the database needs to be clean to run the test
             using (var context = new EFModel.MyContext())
             using (var repository = new Repositories.DicomSeriesRepository(context))
             {
-                // construct a new series object and store the UID
-                var newSeriesDomainModel = TestData.CreateSeries();
-                var newSeriesUid = newSeriesDomainModel.SeriesInstanceUid;
-
                 // perform an update to save it
                 repository.UpdateAsync(newSeriesDomainModel).Wait();
+            }
 
-                string connectionString = 
-                    @"Data Source=(localdb)\ProjectsV13;Initial Catalog=MyStoreDB;";
-
-                // check that the new series exists
-                using (SqlConnection connection = new SqlConnection(connectionString))
+            // check that the new series exists
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string queryString = "select PatientId, Modality, AcquisitionDateTime "
+                    + $"from DicomSeries where SeriesInstanceUID = '{newSeriesUid.ToString()}'";
+                SqlCommand command = new SqlCommand(queryString, connection);
+                connection.Open();
+                using (SqlDataReader reader = command.ExecuteReader())
                 {
-                    string queryString = $"select PatientId, Modality, AcquisitionDateTime from DicomSeries where SeriesInstanceUID = '{newSeriesUid.ToString()}'";
-                    SqlCommand command = new SqlCommand(queryString, connection);
-                    connection.Open();
-                    using (SqlDataReader reader = command.ExecuteReader())
+                    int rowCount = 0;
+
+                    while (reader.Read())
                     {
-                        int rowCount = 0;
+                        rowCount++;
 
-                        while (reader.Read())
-                        {
-                            rowCount++;
+                        var patientId = reader["PatientId"].ToString();
+                        Assert.AreEqual(newSeriesDomainModel.PatientId, patientId);
 
-                            var patientId = reader["PatientId"].ToString();
-                            Assert.AreEqual(newSeriesDomainModel.PatientId, patientId);
+                        var modality = reader["Modality"].ToString();
+                        Assert.AreEqual(newSeriesDomainModel.Modality, modality);
 
-                            var modality = reader["Modality"].ToString();
-                            Assert.AreEqual(newSeriesDomainModel.Modality, modality);
-
-                            var acquisitionDateTime = System.DateTime.Parse(reader["AcquisitionDateTime"].ToString());
-                            Assert.AreEqual(newSeriesDomainModel.AcquisitionDateTime, acquisitionDateTime);
-                        }
-
-                        // should only have returned one row
-                        Assert.AreEqual(rowCount, 1);
+                        var acquisitionDateTime = System.DateTime.Parse(reader["AcquisitionDateTime"].ToString());
+                        Assert.AreEqual(newSeriesDomainModel.AcquisitionDateTime, acquisitionDateTime);
                     }
+
+                    // should only have returned one row
+                    Assert.AreEqual(rowCount, 1);
                 }
             }
         }
@@ -63,16 +78,14 @@ namespace Dicom.Infrastructure.Test
         [TestMethod]
         public void TestAddingInstances()
         {
-            DomainModel.DicomUid newSeriesUid = null;
+            // construct a new series object and store the UID
+            var newSeriesDomainModel = TestData.CreateSeries();
+            var newSeriesUid = newSeriesDomainModel.SeriesInstanceUid;
 
             // NOTE that the database needs to be clean to run the test
             using (var context = new EFModel.MyContext())
             using (var repository = new Repositories.DicomSeriesRepository(context))
             {
-                // construct a new series object and store the UID
-                var newSeriesDomainModel = TestData.CreateSeries();
-                newSeriesUid = newSeriesDomainModel.SeriesInstanceUid;
-
                 // perform an update to save it
                 repository.UpdateAsync(newSeriesDomainModel).Wait();
 
@@ -80,21 +93,53 @@ namespace Dicom.Infrastructure.Test
             }
 
 
+            DomainModel.DicomSeries updateSeriesDomainModel = null;
+
             // now generate a new context / repository
             using (var context = new EFModel.MyContext())
             using (var repository = new Repositories.DicomSeriesRepository(context))
             {
                 // now retreive the series domain model from the repository
-                var updateSeriesDomainModel = repository.GetAggregateForKey(newSeriesUid);
+                updateSeriesDomainModel = repository.GetAggregateForKey(newSeriesUid);
 
                 // and instances to the series
                 TestData.AddInstancesToSeries(updateSeriesDomainModel);
 
                 // and update the result
                 repository.UpdateAsync(updateSeriesDomainModel).Wait();
+            }
 
-                // check that the updated data is present
+            // check that the updated data is present
+            // check that the new series exists
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string queryString = "select SopInstanceUid "
+                    + "from DicomInstances inner join DicomSeries on DicomSeries.ID = DicomInstances.DicomSeriesId "
+                    + $"where DicomSeries.SeriesInstanceUID = '{newSeriesUid.ToString()}'";
 
+                SqlCommand command = new SqlCommand(queryString, connection);
+                connection.Open();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    int rowCount = 0;
+
+                    while (reader.Read())
+                    {
+                        rowCount++;
+
+                        var sopInstanceUid = reader["SopInstanceUid"].ToString();
+
+                        // there should be a single instance that matches the SOP instance UID
+                        var matchInstances =
+                            updateSeriesDomainModel.DicomInstances.Where(instance => 
+                                instance.SopInstanceUid.ToString().CompareTo(sopInstanceUid) == 0)
+                                .ToList();
+                        Assert.AreEqual(matchInstances.Count, 1);
+                    }
+
+                    // should only have returned one row
+                    Assert.AreEqual(rowCount, 3);
+                }
             }
         }
     }
