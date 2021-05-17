ed77908d5147a8b318ca949260ad67b866e4c50f
Lane, Derek G <Derek.Lane@elekta.com>
2021-05-17 08:54:23 -0500

    adding controller test

---
+++ Dicom.Api.Test/DicomControllerTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dicom.Api.Test
+{
+    [TestClass]
+    public class DicomControllerTest
+    {
+        [TestMethod]
+        public void TestCreateSeries()
+        {
+        }
+    }
+}
