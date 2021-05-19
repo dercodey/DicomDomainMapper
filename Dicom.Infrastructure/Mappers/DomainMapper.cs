84439861578b7d4275f213789990c91afb2f7440
Lane, Derek G <Derek.Lane@elekta.com>
2021-05-19 10:00:07 -0500

    add comments about the mapper configuration

--- Dicom.Infrastructure/Mappers/DomainMapper.cs
+++ Dicom.Infrastructure/Mappers/DomainMapper.cs
@@ -25,43 +25,55 @@ namespace Elekta.Capability.Dicom.Infrastructure.Mappers
                     cfg.AddCollectionMappers();
 
                     cfg.CreateMap<DomainModel.DicomSeries, EFModel.DicomSeries>()
+                        // set up collection identity
                         .EqualityComparison((dmDicomSeries, efDicomSeries) =>
                             efDicomSeries.SeriesInstanceUid == dmDicomSeries.SeriesInstanceUid.ToString())
+                        /// ignore primary key and foreign keys
                         .ForMember(efDicomSeries => efDicomSeries.ID, opt => opt.Ignore())
+                        // set up conversion from value objects
                         .ForMember(efDicomSeries => efDicomSeries.SeriesInstanceUid,
                             opt => opt.ConvertUsing(new ToStringFormatter<DomainModel.DicomUid>()));
 
                     cfg.CreateMap<EFModel.DicomSeries, DomainModel.DicomSeries>()
+                        // set up conversion for value objects
                         .ForMember(dmDicomSeries => dmDicomSeries.SeriesInstanceUid, opt => opt.Ignore())
                         .ForCtorParam("seriesInstanceUid",
                             opt => opt.MapFrom(dmDicomSeries =>
                                 new DomainModel.DicomUid(dmDicomSeries.SeriesInstanceUid)));
 
                     cfg.CreateMap<DomainModel.DicomInstance, EFModel.DicomInstance>()
+                        // set up collection identity
                         .EqualityComparison((dmDicomInstance, efDicomInstance) =>
                             efDicomInstance.SopInstanceUid == dmDicomInstance.SopInstanceUid.ToString())
+                        /// ignore primary key and foreign keys
                         .ForMember(efDicomInstance => efDicomInstance.ID, opt => opt.Ignore())
                         .ForMember(efDicomInstance => efDicomInstance.DicomSeriesId, opt => opt.Ignore())
                         .ForMember(efDicomInstance => efDicomInstance.DicomSeries, opt => opt.Ignore())
+                        // set up conversion for value objects
                         .ForMember(efDicomInstance => efDicomInstance.SopInstanceUid,
                             opt => opt.ConvertUsing(new ToStringFormatter<DomainModel.DicomUid>()));
 
                     cfg.CreateMap<EFModel.DicomInstance, DomainModel.DicomInstance>()
+                        // set up conversion for value objects
                         .ForMember(dmDicomInstance => dmDicomInstance.SopInstanceUid, opt => opt.Ignore())
                         .ForCtorParam("sopInstanceUid",
                             opt => opt.MapFrom(dmDicomInstance =>
                                 new DomainModel.DicomUid(dmDicomInstance.SopInstanceUid)));
 
                     cfg.CreateMap<DomainModel.DicomAttribute, EFModel.DicomAttribute>()
+                        // set up collection identity
                         .EqualityComparison((dmDicomAttribute, efDicomAttribute) =>
                             efDicomAttribute.DicomTag == dmDicomAttribute.DicomTag.ToString())
+                        /// ignore primary key and foreign keys
                         .ForMember(efDicomAttribute => efDicomAttribute.ID, opt => opt.Ignore())
                         .ForMember(efDicomAttribute => efDicomAttribute.DicomInstanceId, opt => opt.Ignore())
                         .ForMember(efDicomAttribute => efDicomAttribute.DicomInstance, opt => opt.Ignore())
+                        // set up conversion for value objects
                         .ForMember(efDicomAttribute => efDicomAttribute.DicomTag,
                             opt => opt.ConvertUsing(new ToStringFormatter<DomainModel.DicomTag>()));
 
                     cfg.CreateMap<EFModel.DicomAttribute, DomainModel.DicomAttribute>()
+                        // set up conversion for value objects
                         .ForMember(dmDicomAttribute => dmDicomAttribute.DicomTag, opt => opt.Ignore())
                         .ForCtorParam("dicomTag",
                             opt => opt.MapFrom(dmDicomAttribute =>
